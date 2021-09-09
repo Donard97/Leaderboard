7eccca194756995becb3e15937aa7acd0b5294d2
Donard97 <donardgolaj@gmail.com>
2021-09-09 21:46:40 +0200

    Revert "Add style to all page"
    This reverts commit 503a1e302024e59a38f8a8a4edb8144de48d4ec9.

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,6 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
 };
