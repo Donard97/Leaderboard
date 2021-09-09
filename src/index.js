c3845ee3f62539f6917fa483e6b5a6edd04f14da
Donard97 <donardgolaj@gmail.com>
2021-09-09 15:48:28 +0200

    Add API and functions to send and get scores

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import './style.css';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/10vOjBe9hlFeLKl7vtc7/scores/';
+const refreshBtn = document.getElementById('refreshBtn');
+
+const sendScores = async (url, userName, userScore) => {
+  const gameResult = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      user: userName.value,
+      score: userScore.value,
+    }),
+  });
+  userName.value = '';
+  userScore.value = '';
+  return gameResult.json();
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendScores(url, userName, userScore);
+});
+
+const getScore = async (url) => {
+  const score = await fetch(url);
+  return score.json();
+};
+
+const displayScores = ((scores) => {
+  const scoreArray = scores.result;
+  const scoreDetail = scoreArray.map((b) => `
+    <li>${b.user} : ${b.score}</li>
+  `).join('');
+  document.getElementById('scores-box').innerHTML = `${scoreDetail}`;
+});
+
+refreshBtn.addEventListener('click', () => {
+  getScore(url).then((data) => { displayScores(data); });
+});
